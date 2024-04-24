0a5bf4ce091d53b0f3eeb0f4bf0a71a7472286f7
aamamalia <liaamll278@gmail.com>
2024-04-24 14:20:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/modul5/tab1.java
@@ -0,0 +1,15 @@
+package com.example.modul5;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class tab1 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab1);
+    }
+}
